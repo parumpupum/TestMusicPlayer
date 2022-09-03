28bf2147bfb4c4cc85dd5c632ea716a50531e13b
Parumpupum <parumpupum@mail.ru>
2022-09-03 08:49:13 +0300

    Initial Commit

---
+++ TestMusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMusicPlayer
+//
+//  Created by Kirill Hobyan on 3.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
